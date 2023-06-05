b6fb7b9354dceb67783db7529c29b285ca2c419b
Sandhya devi Saminathan <sandhya021999@gmail.com>
2023-06-05 14:04:41 +0530

    final commit

--- index.js
+++ index.js
@@ -12,10 +12,18 @@ const time = date_obj.getTime();
 const fileName = year + "-" + month + "-" + date + "-" + time;
 //content
 const timestamp = new Date().toISOString();
-
+const render = "https://nodejs-filesystem-gdp2.onrender.com/";
+const github = "https://github.com/Sandhyadevi-Saminathan/nodejs-filesystem";
+const hash = "100644 blob 4fb80a2b2327664640e7068957d383336e2b7c4a ";
+const renders = "\r\n" + render + "\r\n" + github + "\r\n" + hash + "\r\n"
+console.log(renders)
 //file
 const fs = require("fs");
 fs.writeFile(`${fileName}.txt`, timestamp, function (err) { console.log("success") });
+
+
+fs.appendFile(`${fileName}.txt`, renders, function (err) { console.log("success") });
+
 
 //read file
 let files = [];
